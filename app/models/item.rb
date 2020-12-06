8d48fff013d0adee129e5b6207472b3116f1bf35
@t-ichimura-sati0920 <t.ichimura.sati0920@gmail.com>
2020-12-06 12:13:25 +0900

    修正_4

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,10 +16,15 @@ class Item < ApplicationRecord
     validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters." }
     validates :description
   end
+
+  with_options numericality: { other_than: 1 } do
+    validates :status_id
+    validates :cost_id
+    validates :days_id
+    validates :category_id
+    validates :prefacture_id
+  end
+
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
-  validates :status_id, numericality: { other_than: 1 } 
-  validates :cost_id, numericality: { other_than: 1 } 
-  validates :days_id, numericality: { other_than: 1 } 
-  validates :category_id, numericality: { other_than: 1 } 
-  validates :prefacture_id, numericality: { other_than: 1 } 
+  
 end
