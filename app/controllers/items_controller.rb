509c4cff99fff7c24e464ac94a233834302ba6c1
@t-ichimura-sati0920 <t.ichimura.sati0920@gmail.com>
2020-12-07 14:35:46 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,9 +38,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+   if @item.user_id == current_user.id
     if @item.destroy
       redirect_to root_path
     end
+   end
   end
 
   private
