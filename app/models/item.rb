72e161865d229e5ed852e101db8f8513b498e28d
@t-ichimura-sati0920 <t.ichimura.sati0920@gmail.com>
2020-12-14 15:58:46 +0900

    修正(商品画像を1枚つけることが必須)

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,6 +15,7 @@ class Item < ApplicationRecord
     validates :name
     validates :price, numericality: { only_integer: true, message: "is invalid. Input half-width characters." }
     validates :description
+    validates :image
   end
 
   with_options numericality: { other_than: 1 } do
@@ -26,5 +27,9 @@ class Item < ApplicationRecord
   end
 
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range"}
+
+  def image_presence
+    errors.add(:image, 'must be attached') unless image.attached?
+  end
   
 end
